ccc269e1df71653e2afa9912e39278d11706d257
Hariom Sharma <punits152@gmail.com>
2021-09-17 22:09:34 +0530

    Many Changes

--- app_package/forms.py
+++ app_package/forms.py
@@ -1,15 +1,13 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
+from wtforms import StringField, SubmitField
 from wtforms.validators import Email, EqualTo, Length
 
-class RegisterForm(FlaskForm):
-    username = StringField(label="User Name : ", validators=[Length(min=2,max=30)])
-    email_address = StringField(label="Email Address : ",validators=[Email()])
-    password1 = PasswordField(label="Password : ", validators=[Length(min=6)])
-    password2 = PasswordField(label="Confirm Password : ", validators=[EqualTo("password1")])
-    submit = SubmitField(label="Create Account")
-
 class TrainingForm(FlaskForm):
-    data_file_path = StringField(label = "File Path for Training Data ")
-    data_labels_path = StringField(lable = "File Path for Training Data Labels")
+    data_file_path = StringField(label="File Path for Training Data")
+    data_labels_path = StringField(label="File Path for Training Data Labels")
+    submit = SubmitField(label="SUBMIT")
+
+class PredictionForm(FlaskForm):
+    data_file_path = StringField(label="File Path for Prediction Data")
+    data_labels_path = StringField(label="File Path for Prediction Data Labels")
     submit = SubmitField(label="SUBMIT")
--- app_package/routes.py
+++ app_package/routes.py
@@ -1,5 +1,6 @@
+from flask.wrappers import Response
 from werkzeug.utils import redirect
-from app_package.forms import TrainingForm
+from app_package.forms import TrainingForm, PredictionForm
 from flask.templating import render_template
 from app_package import app
 from flask import render_template
@@ -16,15 +17,18 @@ def home_page():
     return render_template("home_page.html")
 
 
-@app.route("/training")
+@app.route("/training",methods=["GET","POST"])
 def training_page():
     # this will take time to run 
 
     """
     Here will be a form that will ask for the path of the file 
+    or the file from internet
     data_path = "data/dorothea_train.data"
-    label_path = "data/dorothea_train.labels"
+    label_path = "data/dorothea_train.labels" 
     """
+    form = TrainingForm()
+
     log_path = "log_files/train_log.txt"
     form = TrainingForm()
     if form.validate_on_submit():
@@ -35,12 +39,27 @@ def training_page():
         train_df,train_labels,test_df,test_labels = data_maker.make_data()
 
         return render_template("prediction_page.html",data_frames = (train_df,train_labels,test_df,test_labels))
+
     elif form.errors != {}:
         for err_msg in form.errors.values():
             print(f"There wan an error with creating user: {err_msg}")
 
-    return render_template("train_page.html")
+    return render_template("train_page.html",form = form)
 
-@app.route("/prediction")
+
+
+@app.route("/prediction",methods=["GET","POST"])
 def prediction_page():
-    return render_template("predict_page.html")
+    form = PredictionForm()
+    if form.validate_on_submit():
+        log_path = "log_files/train_log.txt"
+        
+        data_path = form.data_file_path.data
+        label_path = form.data_labels_path.data
+
+
+        # Here we will predict the labels and will call it predictions and it will be in csv format
+        predictions = "" # it will generate and will use to_csvto generate csv
+
+        return Response(predictions)
+    return render_template("predict_page.html",form = form)
